96027162da940606dd0a4fe1db73d17c27c0e2b1
まいまい <kengsini250@gmail.com>
2021-09-16 16:37:25 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,69 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    label_pos_start = ui->label->pos();
+    label_pos_end.setX(ui->label->width() + label_pos_start.x());
+    label_pos_end.setY(ui->label->height() + label_pos_start.y());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    curr.setX(e->pos().x());
+    curr.setY(e->pos().y());
+    temp.setX(e->pos().x());
+    temp.setY(e->pos().y());
+
+    if(e->pos().x()<label_pos_start.x() || e->pos().x()>label_pos_end.x() ||
+            e->pos().y()<label_pos_start.y() || e->pos().y()>label_pos_end.y())
+    {}
+    else{
+        isPressed = true;
+        curr.setX(e->pos().x());
+        curr.setY(e->pos().y());
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *e)
+{
+    if(isPressed &&
+            (e->pos().x()>label_pos_start.x() && e->pos().x()<label_pos_end.x() &&
+             e->pos().y()>label_pos_start.y() && e->pos().y()<label_pos_end.y())
+      ){
+        temp.setX(e->pos().x());
+        temp.setY(e->pos().y());
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *e)
+{
+    isPressed = false;
+    Rects.push_back(QRect(curr.x(),curr.y(),temp.x()-curr.x(),temp.y()-curr.y()));
+}
+
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+    QPainter p(this);
+    p.setPen(Qt::SolidLine);
+    p.setPen(QColor(255,0,0));
+
+    for(int i=0;i<Rects.size();i++){
+        p.drawRect(Rects[i]);
+    }
+
+    if(isPressed){
+        p.drawRect(QRect(curr.x(),curr.y(),temp.x()-curr.x(),temp.y()-curr.y()));
+    }
+    update();
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QPainter>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void mousePressEvent(QMouseEvent*e)override;
+    void mouseMoveEvent(QMouseEvent*e)override;
+    void mouseReleaseEvent(QMouseEvent*e)override;
+    void paintEvent(QPaintEvent*e)override;
+
+private:
+    Ui::MainWindow *ui;
+    bool isPressed = false;
+    QPoint curr,temp,label_pos_start,label_pos_end;
+    QList<QRect> Rects;
+};
+#endif // MAINWINDOW_H
